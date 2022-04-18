20fd5e2208994e0b203657cfcde5f5f42cfdd7db
josh <joshblondin@cmail.carleton.ca>
2022-04-18 14:22:47 -0400

    commit 1

---
+++ src/main/java/A.java
@@ -0,0 +1,15 @@
+class A{
+    private int num;
+
+    public A(int num) {
+        this.num = num;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
